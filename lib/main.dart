b0084e9fd877fa4f5356b7db9656533f0d07ac2b
FahrizaRamadhan <80759538+FahrizaRamadhan@users.noreply.github.com>
2021-10-13 16:06:32 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,53 @@
 import 'package:flutter/material.dart';
 
-class MyAppBar extends StatelessWidget {
-  const MyAppBar({required this.title, Key? key}) : super(key: key);
-
-  // Fields in a Widget subclass are always marked "final".
-
-  final Widget title;
-
-  @override
+class LayoutRow extends StatelessWidget {
   Widget build(BuildContext context) {
-    return Container(
-      height: 56.0, // in logical pixels
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(color: Colors.blue[500]),
-      // Row is a horizontal, linear layout.
-      child: Row(
-        // <Widget> is the type of items in the list.
-        children: [
-          const IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-            onPressed: null, // null disables the button
-          ),
-          // Expanded expands its child
-          // to fill the available space.
-          Expanded(
-            child: title,
-          ),
-          const IconButton(
-            icon: Icon(Icons.search),
-            // tooltip: 'Search',
-            onPressed: null,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class MyScaffold extends StatelessWidget {
-  const MyScaffold({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    // Material is a conceptual piece
-    // of paper on which the UI appears.
-    return Material(
-      // Column is a vertical, linear layout.
-      child: Column(
-        children: [
-          MyAppBar(
-            title: Text(
-              'Selamat Datang !!!',
-              style: Theme.of(context) //
-                  .primaryTextTheme
-                  .headline6,
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              flexibleSpace: SafeArea(
+                  child: Container(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Spacer(),
+                        Text(
+                          'Kantin Politeknik Kampar',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 25),
+                        ),
+                        Spacer()
+                      ],
+                    )
+                  ],
+                ),
+              )),
             ),
-          ),
-          const Expanded(
-            child: Center(
-              child: Text('Hi Namaku Riza!'),
-            ),
-          ),
-        ],
-      ),
-    );
+            body: Container(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'Nasi Goreng',
+                        textAlign: TextAlign.left,
+                      ),
+                      Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Image.asset('lib/assets/gambar/nasigoreng.jpg'),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )));
   }
 }
 
 void main() {
-  runApp(
-    const MaterialApp(
-      title: 'Tugas_Modul3', // used by the OS task switcher
-      home: SafeArea(
-        child: MyScaffold(),
-      ),
-    ),
-  );
+  runApp(LayoutRow());
 }
